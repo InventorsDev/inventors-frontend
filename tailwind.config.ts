a2b623209c1ce641c9e6ea21dcbcdc8956d24709
Asaolu Elijah <asaoluelijah7@gmail.com>
2024-05-22 18:33:51 +0100

    initial commit

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -13,6 +13,17 @@ const config: Config = {
         'gradient-conic':
           'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
       },
+      colors: {
+        'primary-light': '#E1E5EB',
+        'primary-dark': '#1F2937',
+        'primary-darker': '#111827',
+        'primary-darkest': '#0B1017',
+        'primary-lighter': '#2F3947',
+        'primary-lightest': '#6B7280',
+        'primary-red': '#EF4',
+        'primary-green': '#10B981',
+        'primary-blue': '#06B6D4',
+      }
     },
   },
   plugins: [],
