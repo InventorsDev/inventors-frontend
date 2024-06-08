03efa662f8f7294b778678965c605a226a9d50fe
Omoniyi Opemipo [Electron] <omoniyiopempo6@gmail.com>
2024-06-08 23:07:50 +0100

    Built the get-in-touch section with responsiveness

--- app/components/lp_sections/GetInTouch.tsx
+++ app/components/lp_sections/GetInTouch.tsx
@@ -1,7 +1,13 @@
+import Button from "../Button";
+
 const GetInTouch = () => {
 	return (
-		<div>
-			<h1>Let&apos;s get in touch!</h1>
+		<div className="flex justify-center items-center flex-col gap-6 py-12">
+			<h1 className="text-xl font-bold sm:text-4xl dark:text-white">Let&apos;s solve problems together</h1>
+			<h3 className="text-xs sm:text-lg dark:text-white">Our creativity and learning brings us together</h3>
+			<div className="border border-green-400 rounded-sm transition ease-out duration-300 hover:scale-105">
+				<Button text="Get in Touch" arrow = {true} bgColor="" navBtn={false} textColor="black" href="#"/>
+			</div>
 		</div>
 	);
 };
