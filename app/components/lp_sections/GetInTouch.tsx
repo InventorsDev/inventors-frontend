b7aa1f4b9bc2cffa8369d7a6b862bf296041a48e
Joel Olawanle <57611810+olawanlejoel@users.noreply.github.com>
2024-06-09 20:53:20 +0100

    Update GetInTouch.tsx

--- app/components/lp_sections/GetInTouch.tsx
+++ app/components/lp_sections/GetInTouch.tsx
@@ -1,13 +1,21 @@
-import Button from "../Button";
+import Button from '../Button';
 
 const GetInTouch = () => {
 	return (
 		<div className="flex justify-center items-center flex-col gap-6 py-12">
-			<h1 className="text-xl font-bold sm:text-4xl dark:text-white">Let&apos;s solve problems together</h1>
-			<h3 className="text-xs sm:text-lg dark:text-white">Our creativity and learning brings us together</h3>
-			<div className="border border-green-400 rounded-sm transition ease-out duration-300 hover:scale-105">
-				<Button text="Get in Touch" arrow = {true} bgColor="" navBtn={false} textColor="black" href="#"/>
-			</div>
+			<h2 className="text-xl font-bold lg:text-4xl dark:text-white">
+				Let&apos;s solve problems together
+			</h2>
+			<p className="text-xs font-thin lg:text-lg dark:text-white">
+				Our creativity and learning brings us together
+			</p>
+			<Button
+				text="Get in Touch"
+				arrow={true}
+				textColor="primary-green"
+				transparent={true}
+				href="#"
+			/>
 		</div>
 	);
 };
