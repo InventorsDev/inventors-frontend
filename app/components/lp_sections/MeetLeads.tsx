d87b359af10f58e4ddc1d325ed8a3c4251c3a9dd
olawanlejoel <info4tobest2@gmail.com>
2024-06-13 20:50:33 +0100

    meet leads section

--- app/components/lp_sections/MeetLeads.tsx
+++ app/components/lp_sections/MeetLeads.tsx
@@ -46,7 +46,7 @@ const teamMembers = [
 ];
 
 const MeetLeads = () => {
-	const scrollRef = useRef(null);
+	const scrollRef = useRef<HTMLDivElement>(null);
 
 	const scrollLeft = () => {
 		if (scrollRef.current) {
@@ -61,56 +61,58 @@ const MeetLeads = () => {
 	};
 
 	return (
-		<section className="container py-20">
-			<div className="flex justify-between items-center">
-				<h2 className="text-3xl font-semibold mb-6">
-					MEET OUR <br />
-					LEADERSHIP TEAM
-				</h2>
-				<div className="flex gap-3">
-					<button
-						onClick={scrollLeft}
-						className="border-[1px] text-white border-white rounded-full p-2"
-					>
-						<FaArrowLeft className="font-sm" />
-					</button>
-					<button
-						onClick={scrollRight}
-						className="border-[1px] text-white border-white rounded-full p-2"
-					>
-						<FaArrowRight className="font-base" />
-					</button>
-				</div>
-			</div>
-			<div
-				ref={scrollRef}
-				className="flex overflow-x-auto no-scrollbar scrollbar-hide gap-8"
-			>
-				{teamMembers.map((member, index) => (
-					<div key={index} className="min-w-[300px] flex-shrink-0">
-						<Image
-							src={`/images/${member.image}`}
-							alt={member.name}
-							width={300}
-							height={400}
-							className="object-cover h-80"
-						/>
-						<div className="mt-4 flex justify-between items-start">
-							<div className="">
-								<h3 className="text-xl font-semibold">{member.name}</h3>
-								<p className="text-[#AFB4C2]">{member.title}</p>
-								<p className="text-yellow-400 italic">{member.position}</p>
-							</div>
-							<a
-								href={member.linkedin}
-								aria-label="LinkedIn"
-								className="bg-[#007965] text-white rounded-full p-2"
-							>
-								<FaLinkedinIn />
-							</a>
-						</div>
+		<section className="bg-darkbg text-white">
+			<div className="container py-20 ">
+				<div className="flex justify-between items-center">
+					<h2 className="text-3xl font-semibold mb-6">
+						MEET OUR <br />
+						LEADERSHIP TEAM
+					</h2>
+					<div className="flex gap-3">
+						<button
+							onClick={scrollLeft}
+							className="border-[1px] text-white border-white rounded-full p-2"
+						>
+							<FaArrowLeft className="font-extralight" />
+						</button>
+						<button
+							onClick={scrollRight}
+							className="border-[1px] text-white border-white rounded-full p-2"
+						>
+							<FaArrowRight className="font-base" />
+						</button>
 					</div>
-				))}
+				</div>
+				<div
+					ref={scrollRef}
+					className="flex overflow-x-auto no-scrollbar scrollbar-hide gap-8"
+				>
+					{teamMembers.map((member, index) => (
+						<div key={index} className="min-w-[300px] flex-shrink-0">
+							<Image
+								src={`/images/${member.image}`}
+								alt={member.name}
+								width={300}
+								height={400}
+								className="object-cover h-80"
+							/>
+							<div className="mt-4 flex justify-between items-start">
+								<div className="">
+									<h3 className="text-xl font-semibold">{member.name}</h3>
+									<p className="text-[#AFB4C2]">{member.title}</p>
+									<p className="text-yellow-400 italic">{member.position}</p>
+								</div>
+								<a
+									href={member.linkedin}
+									aria-label="LinkedIn"
+									className="bg-[#007965] text-white rounded-full p-2"
+								>
+									<FaLinkedinIn />
+								</a>
+							</div>
+						</div>
+					))}
+				</div>
 			</div>
 		</section>
 	);
