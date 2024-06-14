457f3f8ab3920b8fe63f50cb62e78d042c29a5be
olawanlejoel <info4tobest2@gmail.com>
2024-06-14 19:56:37 +0100

    partners section

--- app/components/lp_sections/Partners.tsx
+++ app/components/lp_sections/Partners.tsx
@@ -1,56 +1,52 @@
-import Image from "next/image";
-import partnerLogo from "public/images/microsoft-logo.png";
+import Image from 'next/image';
+import whiteLogo from '@/public/images/white-microsoft.png';
+import colorLogo from '@/public/images/color-microsoft.png';
 
 // Sample partners array
 const partners = [
-  {
-    name: "Microsoft",
-    logo: partnerLogo, // Replace with the actual image path
-  },
-  {
-    name: "Microsoft",
-    logo: partnerLogo, // Replace with the actual image path
-  },
-  {
-    name: "Microsoft",
-    logo: partnerLogo, // Replace with the actual image path
-  },
-  {
-    name: "Microsoft",
-    logo: partnerLogo, // Replace with the actual image path
-  },
-  {
-    name: "Microsoft",
-    logo: partnerLogo, // Replace with the actual image path
-  },
+	{
+		name: 'Microsoft',
+	},
+	{
+		name: 'Microsoft',
+	},
+	{
+		name: 'Microsoft',
+	},
+	{
+		name: 'Microsoft',
+	},
+	{
+		name: 'Microsoft',
+	},
 ];
 
 const Partners = () => {
-  return (
-    <section className="container bg-dark-green text-white">
-      <div className="mx-auto">
-        <h3 className="text-xl text-teal-400 mb-2">Partners</h3>
-        <h2 className="text-3xl font-semibold mb-4">
-          <span className="line-through">Companies</span> we work with
-        </h2>
-        <div className="grid grid-cols-2 md:grid-cols-3 lg:grid-cols-5 gap-8">
-          {partners.map((partner, index) => (
-            <div key={index} className="flex flex-col justify-between">
-              <div className="w-24 h-24 relative">
-                <Image
-                  src={partner.logo}
-                  alt={partner.name}
-                  layout="fill"
-                  objectFit="contain"
-                  className="rounded-lg"
-                />
-              </div>
-            </div>
-          ))}
-        </div>
-      </div>
-    </section>
-  );
+	return (
+		<section className="container dark:bg-darkbg text-white bg-[#F7FDFC] pb-10">
+			<div className="">
+				<h3 className="text-2xl text-teal-400 mb-2 font-BrittanySignature">
+					Partners
+				</h3>
+				<h2 className="text-3xl font-semibold mb-4">
+					<span className="line-through">Companies</span> we work with
+				</h2>
+				<div className="grid grid-cols-2 md:grid-cols-3 lg:grid-cols-5 gap-8 mt-10">
+					{partners.map((partner, index) => (
+						<div key={index} className="">
+							<Image
+								src={whiteLogo}
+								alt={partner.name}
+								width={200}
+								height={200}
+								objectFit="contain"
+							/>
+						</div>
+					))}
+				</div>
+			</div>
+		</section>
+	);
 };
 
 export default Partners;
